1287e4feb579e301d6357bde128f35286f5b4c53
gbogd <gbogd@DESKTOP-EIOVJJ8>
2017-12-26 02:21:17 +0200

    IMDB command help and commands

--- src/main/java/dracos/commands/IMDB.java
+++ src/main/java/dracos/commands/IMDB.java
@@ -9,6 +9,32 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class IMDB implements Command{
 
+	private final static HashMap<String, String> subcommands;
+	
+	
+	static {
+		subcommands = new HashMap<String, String>();
+		subcommands.put("comedy", "Searches for comedy movies");
+		subcommands.put("horror", "Searches for horror movies");
+		subcommands.put("action", "Searches for action movies");
+		subcommands.put("drama", "Searches for drama movies");
+		subcommands.put("western", "Searches for western movies");
+		subcommands.put("animation", "Searches for animation movies");
+		subcommands.put("romance", "Searches for romance movies");
+		subcommands.put("fiction", "Searches for fiction movies");
+		subcommands.put("thriller", "Searches for thriller movies");
+		subcommands.put("adventure", "Searches for adventure movies");
+		subcommands.put("scifi", "Searches for scifi movies");
+		subcommands.put("documentary", "Searches for documentary movies");
+		subcommands.put("musical", "Searches for music movies");
+		subcommands.put("historical", "Searches for historical movies");
+		subcommands.put("movie", "Searches for a movie");
+		subcommands.put("actor", "Searches for movies in which this actor played");
+		subcommands.put("blank", "Searches for ");
+		
+		
+		
+	}
 	@Override
 	public boolean called(String raw, String[] args, MessageReceivedEvent event) {
 		// TODO Auto-generated method stub
@@ -31,31 +57,31 @@ public class IMDB implements Command{
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
-		return null;
+		return "all IMDB commands start with this one";
 	}
 
 	@Override
 	public HashMap<String, String> getSubCommands() {
 		// TODO Auto-generated method stub
-		return null;
+		return subcommands;
 	}
 
 	@Override
 	public CommandCategory getCategory() {
 		// TODO Auto-generated method stub
-		return null;
+		return CommandCategory.UTILITY;
 	}
 
 	@Override
 	public CommandScope getScope() {
 		// TODO Auto-generated method stub
-		return null;
+		return CommandScope.PUBLIC;
 	}
 
 	@Override
 	public boolean isListed() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
